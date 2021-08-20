55e6afe273f375432a8dcfe176d202a107b282fc
Grifak <vadim-nepomnyaschii@mail.ru>
2021-08-20 18:28:18 +0300

    add CsvReader

--- src/test/java/com/gr1fak/taskTracker/ApplicationTests.java
+++ src/test/java/com/gr1fak/taskTracker/ApplicationTests.java
@@ -1,13 +1,10 @@
 package com.gr1fak.taskTracker;
 
+import com.gr1fak.taskTracker.csvReader.TaskReaderTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
